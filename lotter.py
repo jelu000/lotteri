18423744cd5298b98e8ddc5245f45fd7f57d4843
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-04-20 11:44:19 +0200

    klar med konsol programm

---
+++ lotter.py
@@ -0,0 +1,34 @@
+import random
+
+class Lotteri:
+    
+    def __init__(self):
+        self.list_vinster = [
+        "Solstol",
+        "Röd Porche",
+        "Handduk",
+        "Ockelbo 500",
+        "10 liter tvål",
+        "BMX cykel",
+        "Surf Bräda ",
+        "Burton Snowboard"
+        "Kawasaki KLX 230 Cross"
+        "ett paket Bregott",
+        "Hawai resa",
+        "en biltvätt på OK",
+        "Kaffe och bulle",
+        "Lyx yatch",
+        "Iphone",
+        "JBL hörlurar",
+        "Makitat skruvdragare",
+        "parfume från Hugo Boss",
+        "konsertbiljett till The Killers",
+        "ett kilo kattmat",
+        "en marsvin"
+        ]
+
+    def get_vinst(self):
+        slumptal = random.randint(0, 19)
+        #print("slump= " +str(slumptal)+ " Längd= " + str(len(self.list_vinster)) )
+        return self.list_vinster[slumptal]
+        
---
+++ main.py
@@ -0,0 +1,33 @@
+import lotter
+
+lotteriet = lotter.Lotteri()
+looping = True
+
+while looping:
+
+    antal_lotter = input("\n\t\tHur många lotter vill du ha? Max 3st! : ")
+
+    
+    try: 
+        int_antal_lotter = int(antal_lotter)
+        i = 0
+        if (int_antal_lotter < 4):
+
+            print("\n\t\tGrattis du vann det här! ")
+            while i < int_antal_lotter:
+                vinst = lotteriet.get_vinst()
+                print("\t\t" + vinst)
+                i += 1 
+            
+    
+        elif (int_antal_lotter > 3):
+            print("\n\t\tDu har valt för många lotter!")
+    except ValueError:
+        print("\n\t\tEndast siffror tillåtet!")
+    
+
+    print("--------------------------------------------------------")
+    spela_igen = input("\n\t\tVill du spela igen? j/n: ")
+    
+    if  (spela_igen == "n" ):
+        break
